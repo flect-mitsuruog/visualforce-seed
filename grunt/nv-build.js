e717dfe33247376c230777b2ad2c727a8dbf6d63
Ogawa Mitsuru <mitsuru.ogawa@flect.co.jp>
2015-07-13 17:45:46 +0900

    Fix buildタスク時にapexPageFlagsが正しく適用されない件

--- grunt/nv-build.js
+++ grunt/nv-build.js
@@ -1,9 +1,9 @@
-module.exports = function(grunt, config) {
+module.exports = function(grunt, defaultConfig) {
   var options = {
     options: {
       inputPath: '<%= config.paths.app %>/',
       outputPath: '<%= config.paths.dist %>/',
-      apexPageFlags: config.apexPageFlags
+      apexPageFlags: defaultConfig.config.apexPageFlags
     }
   };
   return options;
